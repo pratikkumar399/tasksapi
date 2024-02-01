db291c66eaa215da1606b96ea15e7740a626bc86
raipratik <pratikkumar399@gmail.com>
2024-02-01 10:20:14 +0530

    database connectivity done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import express from "express";
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
 
-const app = express();
+import { app } from './app.js';
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+dotenv.config({ path: './.env' });
 
-
-app.listen(3000, () => {
-    console.log('Server on port 3000');
-})
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.error("MongoDb connection failed !", err)
+    });
